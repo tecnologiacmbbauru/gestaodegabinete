9aaa9793dc47098ecb35ab84e1da34800af1a9f8
Paulo Martins <pgsm5@hotmail.com>
2021-01-27 10:51:35 -0300

    alteracao service provider 2- importacoes

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\URL;
+use Illuminate\Support\Facades\Config;
 
 class AppServiceProvider extends ServiceProvider
 {
