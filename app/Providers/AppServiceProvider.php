a48d1a8cde3eddd00f55a623a82aa2bf42419385
Paulo Martins <pgsm5@hotmail.com>
2021-01-27 10:48:27 -0300

    Alteração service provider para url pegar corretamente
    Alteração feita para quando o site tiver em produção aceitar o redirecionamento de url

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,5 +25,9 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
+		URL::forceRootUrl(Config::get('app.url'));
+		if(config('app.env') === 'production') {
+            URL::forceScheme('https');
+        }
     }
 }
