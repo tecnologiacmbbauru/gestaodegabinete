7334ad74d7771ee1ab1560fa4acdae8786360200
Rodrigo Tschope <r_tschope@apple.com>
2023-08-04 20:31:02 +0100

    composer lock php 8.0 small psr-4

--- app/Http/Controllers/SobreController.php
+++ app/Http/Controllers/SobreController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class sobreController extends Controller
+class SobreController extends Controller
 {
     public function index()
     {
