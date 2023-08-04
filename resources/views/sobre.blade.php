b6a99047acd0b845267fc9906be5000f8c066d78
Rodrigo Tschope <r_tschope@apple.com>
2023-08-04 20:03:40 +0100

    Laravel 9 updated

--- resources/views/sobre.blade.php
+++ resources/views/sobre.blade.php
@@ -8,15 +8,20 @@
 <body>
     <h1 class="titulo" style="text-align:center;">Sobre o Sistema</h1>
     <div class="container">
-        <div id="alerta" class="sobre col-md-11">
+        <div id="alerta" class="sobre col-md-12">
             <h3>Gestão de Gabinete</h3>
             <p>
-                <h4>Versão 6.0</h4>
+                <h4>Versão 7.0</h4>
                 <br>
                 <br>
-                Desenvolvido pelo Serviço Tecnológico em Informática
+                Codigo Base foi desenvolvido pelo Serviço Tecnológico em Informática
                 <br>
                 <a href="https://www.bauru.sp.leg.br">Câmara Municipal de Bauru</a> / São Paulo
+                <br>
+                <br>
+                Atual mantedor do projeto Rodrigo Tschope
+                <br>
+                <a href="https://tschope.tech">Rodrigo Tschope</a>
             </p>
             <p style="font-size: 12px;"><br>Ícones feitos por <a href="https://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/br/" title="Flaticon">www.flaticon.com</a></p>
         </div>
